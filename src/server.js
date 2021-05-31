2b5338a1f763c6dcc52dd215d2f087a2c760d907
mahesh <mahesh.giri@cerence.com>
2021-05-31 21:45:52 +0530

    simple post and file read

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import {peoples} from './people';
+import {promises as fs} from 'fs';
+import bodyParser from 'body-parser';
 let app=express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 
 app.get("/hello",(req,res)=>{
@@ -14,6 +20,19 @@ app.get("/people/:name",(req,res)=>{
     let {name}=req.params
     let temppeoples=peoples.filter(item=>item.name.includes(name))
     res.json(temppeoples);
+})
+
+app.get('/filedata',async (req,res)=>{
+let data=await fs.readFile(__dirname+'/peopledata.json');
+let peoples=JSON.parse(data);
+res.json(peoples);
+})
+
+app.post('/people',(req,res)=>{
+let newPerson=req.body;
+peoples.push(newPerson);
+res.json(peoples);
+
 })
 
 app.listen(3000,()=>{
